729660a38a6af7b9b378284516976b4d0a7e78a6
A <krsato@icloud.com>
2025-01-11 16:13:09 +0900

    add new message

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
     char str[] = "Hi!Bob.";
 
-    printf("%s\n", str);
+    printf("%s\nlength: %ld\n", str, strlen(str));
 
     return 0;
 }
